06c28bcd336ecdf170a751a25098ae32d56f61d1
srinivasvasala <vasalasrinivas53@gmail.com>
2024-02-06 15:55:52 +0530

    emplyeeDashboard

--- Frontend/Js/employeeDashboard.js
+++ Frontend/Js/employeeDashboard.js
@@ -8,7 +8,48 @@ document.getElementById('profileImageInput').addEventListener('change', function
     }
   
     reader.readAsDataURL(file);
-  });
+});
+
+
+const month = new Date();
+const options = {
+  month: "long"
+};
+
+const year1 = new Date();
+
+let year = year1.getFullYear();
+
+const date1 = new Date();
+
+let date = date1.getDate();
+
+const day = new Date();
+
+const options2 = {
+  weekday: "long"
+};
+
+console.log(day.toLocaleString("en-IN", options2));
+
+function myFunction() {
+  document.getElementById("month").innerText = month.toLocaleString(
+    "en-IN",
+    options
+  );
+  document.getElementById("year").innerText = year;
+  document.getElementById("date").innerText = date;
+  document.getElementById("day").innerText = day.toLocaleString(
+    "en-IN",
+    options2
+  );
+}
+myFunction();
+
+
+function loadPage(url) {
+  document.getElementById('iframe-content').src = url;
+}
 
   
   
