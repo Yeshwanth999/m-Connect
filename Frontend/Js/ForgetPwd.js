4c290df156bf2c4910f85a21d41204bcd9a660a4
Sandeep Arukala <145935189+SandeepArukala@users.noreply.github.com>
2024-02-09 13:45:21 +0530

    upto verifyotp

--- Frontend/Js/ForgetPwd.js
+++ Frontend/Js/ForgetPwd.js
@@ -10,49 +10,43 @@ function validateForm() {
     } else if (!gmailPattern.test(gmail)) {
         document.getElementById('mail-error').textContent = 'Invalid gmail format.';
     } else {
-
-        const gmail = $('#gmail').val();
-
         const formData = {
-            gmail: gmail,
+            gmail: gmail
         };
         console.log(gmail);
 
         const type = 'POST';
 
-        // Call the updated function for AJAX communication
         sendFormData1('http://localhost:8081/forgotPassword', type, formData, 'formBox2');
 
 
         document.querySelector('.otp-form').style.display = 'block';
     }
 
-    return true; // Return true to indicate validation success
+    return true;
 }
-function validateOTP() {
-    // Reset previous error messages
 
-    // Get values from the OTP form
-    // var element = document.getElementById('otp');
+
+
+function validateOTP() {
+
 
     var otp = document.getElementById('otp').value;
-    // Perform validation (you can add your own validation logic for OTP here)
-    // For now, let's just check if OTP is not empty
+    var gmail = localStorage.getItem("gmail");
     if (otp.trim() === '') {
         document.getElementById('otp-error').textContent = 'OTP is required';
     } else {
-        // Here you can handle further steps like verifying OTP, etc.
     }
     const formData = {
-        otp: otp,
+        gmail: gmail,
+        otp: otp
     };
     console.log(otp);
 
     const type = 'POST'
 
-    verifyOtp('http://localhost:8081/verifyOtp', type, formData, 'formBox2');
+    verifyOtp('http://localhost:8081/verifyOtp?gmail=' + gmail + '&otp=' + otp + '', type, formData, 'formBox2');
 
-    // alert('OTP Verified Successfully!');
 
 
 }
