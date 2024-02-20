bd1f71907ff05b41f23a138109745b2d4785a2e0
Sandeep Arukala <145935189+SandeepArukala@users.noreply.github.com>
2024-02-20 15:05:45 +0530

    my profile ajaxcalling

--- Frontend/Js/myprofile.js
+++ Frontend/Js/myprofile.js
@@ -4,7 +4,7 @@ document.getElementById("lastname").disabled = true;
 document.getElementById("dob").disabled = true;
 document.getElementById("gender").disabled = true;
 document.getElementById("blood_group").disabled = true;
-document.getElementById("marital_status").disabled = true;  
+document.getElementById("marital_status").disabled = true;
 
 
 function toggleEditMode() {
@@ -12,15 +12,15 @@ function toggleEditMode() {
     var selects = document.querySelectorAll('select');
     var buttons = document.querySelector('.button-container');
 
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         input.disabled = !input.disabled;
     });
 
-    selects.forEach(function(select) {
+    selects.forEach(function (select) {
         select.disabled = !select.disabled;
     });
 
- 
+
     buttons.style.display = 'block';
 }
 
@@ -29,11 +29,11 @@ function cancelEdit() {
     var selects = document.querySelectorAll('select');
     var buttons = document.querySelector('.button-container');
 
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         input.disabled = true;
     });
 
-    selects.forEach(function(select) {
+    selects.forEach(function (select) {
         select.disabled = true;
     });
 
@@ -49,16 +49,16 @@ function saveEdit() {
 
 
 // contact info start //
-document.getElementById("email").disabled=true;
-document.getElementById("email1").disabled=true;
-document.getElementById("phonenumber").disabled=true;
-document.getElementById("phonenumber1").disabled=true;
+document.getElementById("email").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("phonenumber").disabled = true;
+document.getElementById("phonenumber1").disabled = true;
 
 function contactOnclick() {
     var inputs = document.querySelectorAll('.input-underlined1');
     var buttons = document.querySelector('.button-container1');
 
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         input.disabled = !input.disabled;
     });
 
@@ -69,7 +69,7 @@ function cancelContact() {
     var inputs = document.querySelectorAll('.input-underlined1');
     var buttons = document.querySelector('.button-container1');
 
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         input.disabled = true;
     });
 
@@ -78,7 +78,7 @@ function cancelContact() {
 }
 
 function saveContact() {
-    
+
     cancelContact();
 }
 // contact info end //
@@ -87,27 +87,27 @@ function saveContact() {
 
 
 // Address info start //
-document.getElementById("currentAddress").disabled=true;
-document.getElementById("currentCountry").disabled=true;
-document.getElementById("currentState").disabled=true;
-document.getElementById("currentCity").disabled=true;
-document.getElementById("currentPincode").disabled=true;
+document.getElementById("currentAddress").disabled = true;
+document.getElementById("currentCountry").disabled = true;
+document.getElementById("currentState").disabled = true;
+document.getElementById("currentCity").disabled = true;
+document.getElementById("currentPincode").disabled = true;
 
-document.getElementById("permanentAddress").disabled=true;
-document.getElementById("permanentCountry").disabled=true;
-document.getElementById("permanentState").disabled=true;
-document.getElementById("permanentCity").disabled=true;
-document.getElementById("permanentPincode").disabled=true;
-document.getElementById("housetype").disabled=true
-document.getElementById("stayingsince").disabled=true
-document.getElementById("livingin").disabled=true
+document.getElementById("permanentAddress").disabled = true;
+document.getElementById("permanentCountry").disabled = true;
+document.getElementById("permanentState").disabled = true;
+document.getElementById("permanentCity").disabled = true;
+document.getElementById("permanentPincode").disabled = true;
+document.getElementById("housetype").disabled = true
+document.getElementById("stayingsince").disabled = true
+document.getElementById("livingin").disabled = true
 
 
 function addressOnclick() {
     var inputs = document.querySelectorAll('.input-underlined2');
     var buttons = document.querySelector('.button-container2');
 
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         input.disabled = !input.disabled;
     });
 
@@ -118,7 +118,7 @@ function cancelAddress() {
     var inputs = document.querySelectorAll('.input-underlined2');
     var buttons = document.querySelector('.button-container2');
 
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         input.disabled = true;
     });
 
@@ -132,16 +132,16 @@ function saveAddress() {
     cancelAddress();
 }
 // social info start //
-document.getElementById("linkedin").disabled=true;
-document.getElementById("twitter").disabled=true;
-document.getElementById("facebook").disabled=true;
+document.getElementById("linkedin").disabled = true;
+document.getElementById("twitter").disabled = true;
+document.getElementById("facebook").disabled = true;
 
 // error for pincode // 
-document.getElementById('currentPincode').addEventListener('blur', function() {
+document.getElementById('currentPincode').addEventListener('blur', function () {
     var pincode = this.value.trim();
-    var pincodeRegex = /^\d{6}$/; 
-    
-    if (pincode-2 === '' || !pincodeRegex.test(pincode-2)) {
+    var pincodeRegex = /^\d{6}$/;
+
+    if (pincode - 2 === '' || !pincodeRegex.test(pincode - 2)) {
         document.getElementById('currentPincode').style.borderBottomColor = '';
         document.getElementById('pincode-error').innerText = 'Please Enter Valid Pincode';
         document.getElementById('pincode-error').style.color = 'red';
@@ -152,14 +152,14 @@ document.getElementById('currentPincode').addEventListener('blur', function() {
 });
 
 
-document.getElementById('permanentPincode').addEventListener('blur', function() {
+document.getElementById('permanentPincode').addEventListener('blur', function () {
     var pincode = this.value.trim();
-    var pincodeRegex = /^\d{6}$/; 
-    
+    var pincodeRegex = /^\d{6}$/;
 
-    if (pincode-2 === '' || !pincodeRegex.test(pincode-2)) {
-        document.getElementById('permanentPincode').style.borderBottomColor = ''; 
-        document.getElementById('pincode-error').innerText = 'Please Enter Valid Pincode'; 
+
+    if (pincode - 2 === '' || !pincodeRegex.test(pincode - 2)) {
+        document.getElementById('permanentPincode').style.borderBottomColor = '';
+        document.getElementById('pincode-error').innerText = 'Please Enter Valid Pincode';
         document.getElementById('pincode-error').style.color = 'red';
     } else {
         document.getElementById('permanentPincode').style.borderBottomColor = '#ccc';
@@ -206,7 +206,7 @@ function socialOnclick() {
     var inputs = document.querySelectorAll('.input-underlined3');
     var buttons = document.querySelector('.button-container3');
 
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         input.disabled = !input.disabled;
     });
 
@@ -217,7 +217,7 @@ function cancelsocial() {
     var inputs = document.querySelectorAll('.input-underlined3');
     var buttons = document.querySelector('.button-container3');
 
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         input.disabled = true;
     });
 
@@ -254,10 +254,10 @@ function copyAddress() {
 }
 
 // Enable checkbox if all current address inputs are filled
-document.querySelectorAll('.currentaddress').forEach(function(input) {
-    input.addEventListener('input', function() {
+document.querySelectorAll('.currentaddress').forEach(function (input) {
+    input.addEventListener('input', function () {
         var allInputsFilled = true;
-        document.querySelectorAll('.currentaddress').forEach(function(input) {
+        document.querySelectorAll('.currentaddress').forEach(function (input) {
             if (input.value.trim() === '') {
                 allInputsFilled = false;
             }
@@ -268,54 +268,22 @@ document.querySelectorAll('.currentaddress').forEach(function(input) {
 
 
 // tranfering data using json// 
-$('#saveEdit').on('click', function (){
+/***********$('#saveEdit, #saveContact, #saveAddress, #savesocial').on('click', function (){
+    // personal info start
     var firstname = $('#firstname').val();
-    var secondname = $('#secondname').val();    
+    var lastname = $('#lastname').val();    
     var dob = $('#dob').val();
     var gender = $('#gender').val();
     var blood_group = $('#blood_group').val();
+    var isAdmin = $('#adminCheckbox').is(':checked');
 
-    jsonObject = {
-        "firstname":"",
-        "secondname":"",
-        "dob":"",
-        "gender":"",
-        "blood_group":"",
-    }
-    jsonObject.firstname=firstname
-    jsonObject.secondname=secondname
-    jsonObject.dob=dob
-    jsonObject.gender=gender
-    jsonObject.blood_group=blood_group
-
-    var str = JSON.stringify(jsonObject);
-    document.write(str);
-
-});
-
-$('#saveContact').on('click', function (){
+    // contact info start
     var email = $('#email').val();
-    var email1 = $('#email1').val();
+    var password = $('#password').val();
     var phonenumber = $('#phonenumber').val();
     var phonenumber1 = $('#phonenumber1').val();
 
-    jsonObject = {
-        "email":"",
-        "email1":"",
-        "phonenumber":"",
-        "phonenumber1":"",
-    }
-    jsonObject.email=email
-    jsonObject.email1=email1
-    jsonObject.phonenumber=phonenumber
-    jsonObject.phonenumber1=phonenumber1
-
-    var str = JSON.stringify(jsonObject);
-    document.write(str);
-
-});
-
-$('#saveAddress').on('click', function (){
+    // Address info start
     var currentAddress = $('#currentAddress').val();
     var currentCountry = $('#currentCountry').val();
     var currentState = $('#currentState').val();
@@ -328,8 +296,27 @@ $('#saveAddress').on('click', function (){
     var permanentCity = $('#permanentCity').val();
     var permanentPincode = $('#permanentPincode').val();
 
+    // social info start
+    var linkedin = $('#linkedin').val();
+    var twitter = $('#twitter').val();
+    var facebook = $('#facebook').val();
 
     jsonObject = {
+        // personal info start
+        "firstname":"",
+        "lastname":"",
+        "dob":"",
+        "gender":"",
+        "blood_group":"",
+        "isAdmin":"",
+
+        // contact info start
+        "email":"",
+        "password":"",
+        "phonenumber":"",
+        "phonenumber1":"",
+
+        // Address info start
         "currentAddress":"",
         "currentCountry":"",
         "currentState":"",
@@ -340,7 +327,27 @@ $('#saveAddress').on('click', function (){
         "permanentState":"",
         "permanentCity":"",
         "permanentPincode":"",
+
+        // social info start
+        "linkedin":"",
+        "twitter":"",
+        "facebook":"",
     }
+    // personal info start
+    jsonObject.firstname=firstname
+    jsonObject.lastname=lastname
+    jsonObject.dob=dob
+    jsonObject.gender=gender
+    jsonObject.blood_group=blood_group
+    jsonObject.isAdmin=isAdmin
+
+    // contact info start
+    jsonObject.email=email
+    jsonObject.password=password
+    jsonObject.phonenumber=phonenumber
+    jsonObject.phonenumber1=phonenumber1
+
+    // Address info start
     jsonObject.currentAddress=currentAddress
     jsonObject.currentCountry=currentCountry
     jsonObject.currentState=currentState
@@ -352,30 +359,110 @@ $('#saveAddress').on('click', function (){
     jsonObject.permanentCity=permanentCity
     jsonObject.permanentPincode=permanentPincode
 
-    var str = JSON.stringify(jsonObject);
-    document.write(str);
-
-});
-
-$('#savesocial').on('click', function (){
-    var linkedin = $('#linkedin').val();
-    var twitter = $('#twitter').val();
-    var facebook = $('#facebook').val();
-
-
-    jsonObject = {
-        "linkedin":"",
-        "twitter":"",
-        "facebook":"",
-
-    }
+    // social info start
     jsonObject.linkedin=linkedin
     jsonObject.twitter=twitter
     jsonObject.facebook=facebook
 
-
     var str = JSON.stringify(jsonObject);
     document.write(str);
 
-});
+});     ********************/
 
+$('#saveEdit, #saveContact, #saveAddress, #savesocial').on('click', function () {
+    // personal info start
+    var firstname = $('#firstname').val();
+    var lastname = $('#lastname').val();
+    var dob = $('#dob').val();
+    var gender = $('#gender').val();
+    var blood_group = $('#blood_group').val();
+    var isAdmin = $('#adminCheckbox').is(':checked');
+
+    // contact info start
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var phonenumber = $('#phonenumber').val();
+    var phonenumber1 = $('#phonenumber1').val();
+
+    // Address info start
+    var currentAddress = $('#currentAddress').val();
+    var currentCountry = $('#currentCountry').val();
+    var currentState = $('#currentState').val();
+    var currentCity = $('#currentCity').val();
+    var currentPincode = $('#currentPincode').val();
+
+    var permanentAddress = $('#permanentAddress').val();
+    var permanentCountry = $('#permanentCountry').val();
+    var permanentState = $('#permanentState').val();
+    var permanentCity = $('#permanentCity').val();
+    var permanentPincode = $('#permanentPincode').val();
+
+    // social info start
+    var linkedin = $('#linkedin').val();
+    var twitter = $('#twitter').val();
+    var facebook = $('#facebook').val();
+
+    const empObject = {
+        // personal info start
+        firstname: firstname,
+        lastname: lastname,
+        dob: dob,
+        gender: gender,
+        blood_group: blood_group,
+        isAdmin: isAdmin,
+        // personal info start
+        email: email,
+        password: password,
+        phonenumber: phonenumber,
+        phonenumber1: phonenumber1,
+        // Address info start
+        currentAddress: currentAddress,
+        currentCountry: currentCountry,
+        currentState: currentState,
+        currentCity: currentCity,
+        currentPincode: currentPincode,
+        permanentAddress: permanentAddress,
+        permanentCountry: permanentCountry,
+        permanentState: permanentState,
+        permanentCity: permanentCity,
+        permanentPincode: permanentPincode,
+        // social info start
+        linkedin: linkedin,
+        twitter: twitter,
+        facebook: facebook,
+    };
+    // personal info start
+    console.log(firstname);
+    console.log(lastname);
+    console.log(dob);
+    console.log(gender);
+    console.log(blood_group);
+    console.log(isAdmin);
+    // personal info start
+    console.log(email);
+    console.log(password);
+    console.log(phonenumber);
+    console.log(phonenumber1);
+    // Address info start
+    console.log(currentAddress);
+    console.log(currentCountry);
+    console.log(currentState);
+    console.log(currentCity);
+    console.log(currentPincode);
+    console.log(permanentAddress);
+    console.log(permanentCountry);
+    console.log(permanentState);
+    console.log(permanentCity);
+    console.log(permanentPincode);
+    // social info start
+    console.log(linkedin);
+    console.log(twitter);
+    console.log(facebook);
+
+
+    const type = 'POST';
+
+    // Call the updated function for AJAX communication
+    sendEmpData(type, empObject, 'form');
+
+});
