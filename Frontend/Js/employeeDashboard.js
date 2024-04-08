8fd9cdc43846cdba267240b50736f738844c7abe
RajeshBaluAddala <rajeshbalu.addala@gmail.com>
2024-04-08 15:15:25 +0530

    added some basic modifications to all these pages

--- Frontend/Js/employeeDashboard.js
+++ Frontend/Js/employeeDashboard.js
@@ -1,109 +1,90 @@
-var usermail = sessionStorage.getItem("usermail");
-console.log(usermail);
+//  // sessionStorage.setItem("usermail","rajeshbalu.addala@gmail.com")
+// var usermail = sessionStorage.getItem("usermail");
+// console.log(usermail);
 
-if (usermail != " " && usermail !== null) {
-  geturlLink = 'http://localhost:8081/user/getprofileimage/' + usermail + '';
+// if (usermail != " " && usermail !== null) {
+//   //geturlLink = 'http://localhost:8081/user/getprofileimage/' + usermail + '';
 
-  geturlLink1forName = 'http://localhost:8081/user/getemployee/' + usermail + '';
+//   geturlLink1forName = 'http://localhost:8081/user/getemployee/' + usermail + '';
 
-  console.log(usermail + "   : " + "Link :" + geturlLink);
+//   console.log(usermail + "   : " + "Link :" + geturlLink);
 
-} else {
-  console.log("gmail is not found.");
-}
+// } else {
+//   console.log("gmail is not found.");
+// }
 
-// $(document).ready(function () {
-//   // Fetch image data from the server when the page loads
+
+jQuery(document).ready(function () {
+  var geturlLink = "https://picsum.photos/seed/picsum/200/300";
+  // Fetch data from the server when the page loads
+  $.ajax({
+    type: 'GET',
+    url: geturlLink,
+    success: function (response) {
+      console.log("Response:", response);
+      var image=$("<img>").attr("src",geturlLink).attr("alt", "Dog Image");
+      $('#imageContainer').html(image);
+
+    },
+    error: function (error) {
+      console.error('Error fetching profile image:', error);
+    }
+  });
+});
+
+
+
+// jQuery(document).ready(function () {
+//   // Fetch data from the server when the page loads
 //   $.ajax({
-//     type: "GET",
+//     type: 'GET',
 //     url: geturlLink,
-//     responseType: "arraybuffer",
+//     xhrFields: {
+//       responseType: 'arraybuffer'
+//     },
 //     success: function (response) {
-//       if (response) {
-//         // Convert the binary data to base64 string
-//         var binaryData = new Uint8Array(response);
-//         var imageData = btoa(String.fromCharCode.apply(null, binaryData));
+//       console.log("Response:", response);
 
-//         // Set the src attribute of a new image element
-//         var imageUrl = "data:image/jpeg;base64," + imageData;
-//         var imageElement = $("<img>").attr("src", imageUrl);
+//       // Convert ArrayBuffer to Blob
+//       const blob = new Blob([response], { type: 'image/jpeg' });
 
-//         // Append the image element to the imageContainer
-//         var $imageContainer = $("#imageContainer");
-//         if ($imageContainer.length) {
-//           $imageContainer.append(imageElement);
-//           console.log("Image added successfully.");
-//           console.log(imageUrl);
-//           console.log(imageElement);
-//         } else {
-//           console.error("Image container element not found");
-//         }
-//       } else {
-//         console.error("No image data found in the response");
-//       }
+//       // Create an object URL from the Blob
+//       const imageUrl = URL.createObjectURL(blob);
+
+//       // Display the image in an <img> element with id 'profileImage'
+//       $('#profileImage').attr('src', imageUrl);
+
 //     },
 //     error: function (error) {
-//       console.error("Error fetching profile image:", error);
-//     },
+//       console.error('Error fetching profile image:', error);
+//     }
 //   });
 // });
 
 
-$(document).ready(function () {
-  // Fetch image data from the server when the page loads
-  $.ajax({
-    type: "GET",
-    url: geturlLink, // Make sure this is the correct URL
-    // responseType: "arraybuffer", // Comment this line if not needed
-    success: function (response) {
-      if (response) {
-        // Convert the binary data to base64 string
-        var imageData = response; // Assuming the server returns base64 directly
-
-        // Set the src attribute of a new image element
-        var imageUrl = "data:image/jpeg;base64," + imageData;
-        var imageElement = $("<img>").attr("src", imageUrl);
-
-        // Append the image element to the imageContainer
-        var $imageContainer = $("#imageContainer");
-        if ($imageContainer.length) {
-          $imageContainer.append(imageElement);
-          console.log("Image added successfully.");
-          console.log(imageUrl);
-          console.log(imageElement);
-        } else {
-          console.error("Image container element not found");
-        }
-      } else {
-        console.error("No image data found in the response");
-      }
-    },
-    error: function (error) {
-      console.error("Error fetching profile image:", error);
-    },
-  });
-});
 
 
 
 
-$(document).ready(function () {
-  // Fetch data from the server when the page loads
-  $.ajax({
-    type: 'GET',
-    url: geturlLink1forName, // Replace 'user/data' with your actual endpoint
-    success: function (responseData) {
-      // Populate the form fields with the retrieved data
-      //$('#firstname').val(responseData.firstname);
-      //console.log(firstname);
 
-      $('#firstnameDisplay').text(responseData.firstname);
-    },
-    error: function (error) {
-      console.error('Error fetching data:', error);
-    }
-  });
-});
+
+// $(document).ready(function () {
+//   // Fetch data from the server when the page loads
+//   $.ajax({
+//     type: 'GET',
+//     url: geturlLink1forName, // Replace 'user/data' with your actual endpoint
+//     success: function (responseData) {
+//       // Populate the form fields with the retrieved data
+//       //$('#firstname').val(responseData.firstname);
+//       //console.log(firstname);
+
+//       $('#firstnameDisplay').text(responseData.firstname);
+//     },
+//     error: function (error) {
+//       console.error('Error fetching data:', error);
+//     }
+//   });
+// });
 
 
 $('#logoutButton').on('click', function () {
